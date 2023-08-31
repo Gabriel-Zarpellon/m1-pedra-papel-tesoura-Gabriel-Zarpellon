0bf2680bf64d6aeada2a71d7d436eb2d44de0dc3
hilariohabhab <hilariohabhab@hotmail.com>
2023-08-30 21:05:08 -0300

    Teste resolvido

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(playerOne, playerTwo) {
+  if (playerOne === playerTwo) {
+    return "Empate!";
+  }
+  if (
+    (playerOne === "Pedra" && playerTwo === "Tesoura") ||
+    (playerOne === "Papel" && playerTwo === "Pedra") ||
+    (playerOne === "Tesoura" && playerTwo === "Papel")
+  ) {
+    return "Jogador 1 venceu!";
+  }
+  {
+    return "Jogador 2 venceu!";
+  }
+}
+playRockPaperScissor();
